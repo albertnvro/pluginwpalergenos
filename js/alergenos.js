10e9aefb1731df34a0650df2420988fafacafb7a
albertnvro <probatch@gmail.com>
2024-06-07 03:03:08 +0200

    Add files via upload

---
+++ js/alergenos.js
@@ -0,0 +1,8 @@
+// js/alergenos.js
+document.addEventListener('DOMContentLoaded', function() {
+    tippy('.alergeno-icon', {
+        theme: 'custom',
+        placement: 'top',
+        trigger: 'mouseenter click',
+    });
+});
